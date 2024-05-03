c1ff2498bca5f5be8d8663ce14eaa6cceb8eb42e
Venkatasai Visweswar Karanam <vkaranam@manh.com>
2024-05-04 00:46:04 +0530

    first commit

--- account-app/src/main/java/com/brodygaudel/accountservice/controller/RestConsumerInterface.java
+++ account-app/src/main/java/com/brodygaudel/accountservice/controller/RestConsumerInterface.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "http://feignrest-svc:9090", value="customer-service")
+@FeignClient(url = "http://customer-svc:9090", value="customer-service")
 public interface RestConsumerInterface {
 	@RequestMapping(method = RequestMethod.GET, value="/v2/customers/hello")
 	public String getHello();
